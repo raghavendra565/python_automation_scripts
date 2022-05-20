926ebedf1b92665f3a1bb72f82346c182cb31746
Karrothu <karrothuraghavendra@gmail.com>
2022-05-20 19:30:32 +0530

    chunks functionality added

--- generate_json.py
+++ generate_json.py
@@ -5,44 +5,37 @@ import json
 
 # Main Path of Lua files
 main_path = os.getcwd()
-# Lua Chunk files Path
-os.chdir("..")
-child_path = os.getcwd()
-
-# Changing directory to main path
-os.chdir(main_path)
 
 files_details = []
 for file in glob.glob("*.lua"):
+    CHUNK_SIZE = 1000
+    file_number = 0
+    with open(file) as f:
+        chunk = f.read(CHUNK_SIZE)
+        while chunk:
+            with open(file + str(file_number), "w") as chunk_file:
+                chunk_file.write(chunk)
+            file_number += 1
+            chunk = f.read(CHUNK_SIZE)
+
     # Reading a lua file
     with open(file, "r") as fh:
         file_content = fh.read()
+
         # Finding version in lua file
         match_string = re.search('function(\s)([a-zA-Z0-9_\./\\-]*).version\(\)(\s+)return(\s)"([A-Za-z0-9_\./\\-]*)"(\s+)end', file_content).group()
         version = re.search('"([A-Za-z0-9_\./\\-]*)"', match_string).group()
         version = version.strip('"')
-    fd = [file, os.path.getsize(file), version]
-    os.chdir(child_path)
-    ch_files_count = 0
-    for ch_file in glob.glob(file + "*"):
-        print(ch_file)
-        ch_files_count += 1
-    os.chdir(main_path)
-    fd.append(ch_files_count)
+
+    fd = [file, os.path.getsize(file), version, file_number]
+    # ch_files_count = 0
+    # for ch_file in glob.glob(file + "*"):
+    #     ch_files_count += 1
+    # fd.append(ch_files_count)
     files_details.append(fd)
+    # break
 
 # Writing output to json file
 with open("LuaFileVersions.json", "w", encoding='utf-8') as fh:
     json.dump({"available_files": files_details}, fh, ensure_ascii=False, indent=4)
 
-# function scheduler_02.version()
-# 	return "2.0"
-# end
-
-# function powerfail.version()
-# 	return "1.0.2"
-# end
-
-# function api32.version()
-# 	return "2.0"
-# end
